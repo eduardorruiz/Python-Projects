20ae871976f0c1b4346faada475a705fb25a6444
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-03-28 19:40:11 -0300

    Organizando

--- Aulas/teste.py
+++
@@ -1,9 +0,0 @@
-A = float(input('Digite o valor de A: '))
-B = float(input('Digite o valor de B: '))
-C = float(input('Digite o valor de C: '))
-Delta = B ** 2 - 4 * A * C
-print(Delta)
-X1 = ((-1 * B) + (Delta ** 0.5)) / 2 * A
-#X2 = (-1 * B) - (Delta ** 0.5) / (2 * A)
-print(X1)
-#print(X2)
