0a8570e526fb81d75b3d432af3def7e286b0e2ad
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-05-02 17:58:15 -0300

    ex86

--- Curso de Python/exrs/ex86.py
+++ Curso de Python/exrs/ex86.py
@@ -2,3 +2,11 @@
 No final, mostre a matriz na tela, com a formatação correta."""
 
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        matriz[linha][coluna] = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
+print('-~' * 16)
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f'[{matriz[linha][coluna]:^5}]', end='')
+    print()
