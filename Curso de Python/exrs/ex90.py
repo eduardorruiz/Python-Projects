641286e4c9e08010649789c9dfd8299e960d3fcc
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-05-12 15:58:26 -0300

    ex90

--- Curso de Python/exrs/ex90.py
+++ Curso de Python/exrs/ex90.py
@@ -1,14 +1,14 @@
 """ Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando
 também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela."""
 dados = {}
-lista = list()
 dados['nome'] = str(input('Digite seu nome: ')).strip()
 média = float(input('Digite sua média: '))
 dados['média'] = média
 if média > 6:
     dados['Situação'] = 'APROVADO'
+elif:
+    dados['Situação'] = 'RECUPERAÇÃO'
 else:
-    dados['Situação'] = 'de RECUPERAÇÃO'
+    dados['Situação'] = 'REPROVADO'
 print(dados)
-lista.append(dados.copy())
 print(f'O aluno {dados['nome']}, tem a média {dados['média']} e está {dados['Situação']}')
