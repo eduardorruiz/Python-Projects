3083b05beef2dd8c90f48937f4f8b7af3ff9000c
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-05-19 16:54:45 -0300

    ex97

---
+++ Curso de Python/exrs/ex97.py
@@ -0,0 +1,12 @@
+""" Faça um programa que tenha uma função chamada escreva(), que receba
+um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável"""
+
+def escreva(txt):
+    tam = len(txt) + 4
+    print('-' * tam)
+    print(f'  {txt}')
+    print('-' * tam)
+
+# Programa Principal
+texto = str(input('Digite algo: '))
+escreva(texto)
