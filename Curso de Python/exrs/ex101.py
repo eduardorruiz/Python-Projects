4e42c49f009b4757127428708ab06e1c53459126
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-08-04 22:45:05 -0300

    ex 101

--- Curso de Python/exrs/ex101.py
+++ Curso de Python/exrs/ex101.py
@@ -1,2 +1,17 @@
-"""Exercício Python 101: Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de
-uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições."""
+""" Exercício Python 101: Crie um programa que tenha uma função chamada voto() que vai
+receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal
+indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições."""
+
+def voto():
+    from datetime import date
+    ano = date.today().year
+    nascimento = int(input('Digite o ano que você nasceu: '))
+    idade = ano - nascimento
+    if idade < 16:
+        resposta = f'Com {idade} anos não vota, NEGADO!'
+    elif 16 <= idade <= 18 or idade > 65:
+        resposta = f'Com {idade} anos pode votar, OPCIONAL!'
+    else:
+        resposta = f'Com {idade} anos deve votar, OBRIGATÓRIO!'
+    return resposta
+print(voto())
