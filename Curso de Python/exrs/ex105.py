d90427df186b95c1329f37c8f869dca60f93e542
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-08-06 00:18:52 -0300

    ex 105

---
+++ Curso de Python/exrs/ex105.py
@@ -0,0 +1,25 @@
+""" Exercício Python 105: Faça um programa que tenha uma função notas() que pode receber
+várias notas de alunos e vai retornar um dicionário com as seguintes informações:
+Quantidade de notas, A maior nota, A menor nota, A média da turma, A situação"""
+
+def notas(*n, sit=False):
+    """ --> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos.
+    :param sit: valor opicional, indicando se deve ou não adicionar a situação
+    :return: dicionário com várias informações sobre a situação da turma."""
+    notas = {}
+    notas['total'] = len(n)
+    notas['maior'] = max(n)
+    notas['menor'] = min(n)
+    notas['média'] = sum(n)/len(n)
+    if notas['média'] >= 7:
+        notas['situação'] = 'BOA'
+    elif notas['média'] >= 5:
+        notas['situação'] = 'RAZOÁVEL'
+    else:
+        notas['situação'] = 'RUIM'
+    return notas
+#Prog Principal
+resp = notas(6, 8, 10, sit=True)
+help(notas)
+print(resp)
