89f2b559b8c7abd97b34d561508365547e654bf8
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-08-07 17:32:58 -0300

    funçao resumo

--- Curso de Python/Módulos/moeda/__init__.py
+++ Curso de Python/Módulos/moeda/__init__.py
@@ -10,5 +10,16 @@ def dobro(preço= 0):
 def metade(preço= 0):
     return preço / 2
 
-def moeda(preço = 0, moeda = 'R$'):
+def formatação(preço = 0, moeda = 'R$'):
     return f'{moeda}{preço:.2f}'.replace('.', ',')
+
+def resumo(preço= 0, txa = 0, txd = 0):
+    print('-'*30)
+    print('RESUMO DO VALOR'.center(30))
+    print('-'*30)
+    print(f'Preço analisado: {formatação(preço)}')
+    print(f'Dobro do preço:  {formatação(dobro(preço))}')
+    print(f'Metade do preço: {formatação(metade(preço))}')
+    print(f'{txa}% de aumento:  {formatação(aumentar(preço, txa))}')
+    print(f'{txd}% de redução:  {formatação(diminuir(preço, txd))}')
+    print('-'*30)
