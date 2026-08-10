3e2e95c36c725a80f66de0a4e0c01141eb35a550
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-08-10 20:58:13 -0300

    ex 113

--- Curso de Python/exrs/ex113.py
+++ Curso de Python/exrs/ex113.py
@@ -1,19 +1,5 @@
-def leiadinheiro(mensagem):
-    válido = False
-    while not válido:
-        entrada = str(input(mensagem)).replace(',', '.').strip()
-        if entrada.isalpha() or entrada == '':
-            print('ERRO: "" é um preço inválido!')
-        else:
-            válido = True
-            return float(entrada)
-
-def leiaint(mensagem):
-    n = input(mensagem).strip()
-    while not n.isnumeric():
-        print('ERRO, digite um número inteiro válido!')
-        n = input(mensagem).strip()
-    return int(n)
+""" Exercício Python 113: Reescreva a função leiaInt() que fizemos no desafio 104, incluindo agora a possibilidade
+ da digitação de um número de tipo inválido. Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade."""
 
 def leiaInt(mensagem):
     while True:
@@ -21,7 +7,7 @@ def leiaInt(mensagem):
             n = int(input(mensagem))
         except (ValueError, TypeError):
             print('Erro: por favor, digite um número inteiro válido')
-            continue  # joga pro laço denovo.
+            continue # joga pro laço denovo.
         except (KeyboardInterrupt):
             print('\nEntrada de dados interrompida pelo usuário')
             return 0
@@ -40,3 +26,6 @@ def leiaFloat(mensagem):
             return 0
         else:
             return n
+n = leiaInt('Digite um inteiro: ')
+n2 = leiaFloat('Digite um real: ')
+print(f'Você acabou de digitar o número {n} e o real foi {n2}.')
