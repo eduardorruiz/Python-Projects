d86703907d72244dc213f71ffa7bdca0bd611874
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-08-11 15:02:27 -0300

    update ex115

--- Curso de Python/exrs/ex115a.py
+++ Curso de Python/exrs/ex115a.py
@@ -1,12 +1,17 @@
 """Exercício Python 115a: Vamos criar um menu em Python, usando modularização."""
-from Módulos.ex115 import *
+from Módulos.ex115.interface import *
+from Módulos.ex115.arquivo import *
 from time import sleep
 
+arq = 'ex115arquivo.txt'
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 OK = False
 while not OK:
     resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Novas Pessoas', 'Sair do Sistema'])
     if resposta == 1:
-        cabeçalho('OP 1')
+        # Opção de listar o conteúdo de um arquivo
+        lerArquivo(arq)
     elif resposta == 2:
         cabeçalho('OP 2')
     elif resposta == 3:
