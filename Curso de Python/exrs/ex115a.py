0cab63083e4ca07b1a3fc2f331e081ecfe299a26
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-08-11 15:06:25 -0300

    update 115c

--- Curso de Python/exrs/ex115a.py
+++ Curso de Python/exrs/ex115a.py
@@ -1,4 +1,6 @@
-"""Exercício Python 115a: Vamos criar um menu em Python, usando modularização."""
+"""Exercício Python 115a: Vamos criar um menu em Python, usando modularização.
+Exercício Python 115b: Vamos ver como fazer acesso a arquivos usando o Python.
+Exercício Python 115c: Vamos finalizar o projeto de acesso a arquivos em Python."""
 from Módulos.ex115.interface import *
 from Módulos.ex115.arquivo import *
 from time import sleep
--- Curso de Python/exrs/ex115b.py
+++
@@ -1,1 +0,0 @@
-""" Exercício Python 115b: Vamos ver como fazer acesso a arquivos usando o Python. """
