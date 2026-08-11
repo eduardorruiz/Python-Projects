bff89490b84320b2dc17e6affd5c3d3b5433c19b
Eduardo Ruiz <eduardorruiz.0@gmail.com>
2026-08-11 15:25:32 -0300

    Ex 115 Terminado

--- Curso de Python/Módulos/ex115/arquivo/__init__.py
+++ Curso de Python/Módulos/ex115/arquivo/__init__.py
@@ -25,4 +25,25 @@ def lerArquivo(nome):
         print('Erro ao ler arquivo!')
     else:
         cabeçalho('PESSOAS CADASTRADAS')
-        print(arquivo.read())
+        contador = 0
+        for linha in arquivo:
+            contador += 1
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{contador}.{dado[0]:<25} {dado[1]:>3} anos')
+    finally:
+        arquivo.close()
+
+def cadastrar(arquivo, nome = 'desconhecido', idade = 0):
+    try:
+        arquivo = open(arquivo, 'at')
+    except:
+        print('Erro na abertura do arquivo!')
+    else:
+        try:
+            arquivo.write(f'{nome}; {idade}\n')
+        except:
+            print('Erro na escrita dos dados!')
+        else:
+            print(f'Novo registro de {nome} adicionado!')
+
--- Curso de Python/exrs/ex115a.py
+++ Curso de Python/exrs/ex115a.py
@@ -15,7 +15,11 @@ while not OK:
         # Opção de listar o conteúdo de um arquivo
         lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('OP 2')
+        # Opção de cadastrar uma nova pessoa
+        cabeçalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do sistema... Até logo!')
         OK = True
